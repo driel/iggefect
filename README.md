c33d88bc63d01e7e16d1d2b94c94bf305237fea3
Dariel <dariel87@dr.iel>
2014-09-20 14:59:49 +0700

    final commit for v1.0

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 iggeffect jQuery plugin for image effect
+
+usage:
+<img src="anything.jpg|png|gif|bmp" id="the_id" alt="">
+<pre>
+	$(function(){
+		$('#the_id').iggefect('original'); // could be original | grayscale | invert | noise | brightness | treshold | sepia | darken | sharpen | emboss | blur
+	});
+</pre>
+
+credit :
+<a href="http://www.html5rocks.com/en/tutorials/canvas/imagefilters/">htmlrocks</a> - for convolute implementation
--- iggefect.js
+++ iggefect.js
@@ -52,52 +52,6 @@
 			return output;
 		}
 
-		this.convolute32 = function(pixels, weights, opaque){
-			var side = Math.round(Math.sqrt(weights.length));
-			var halfSide = Math.floor(side/2);
-
-			var src = pixels.data;
-			var sw = pixels.width;
-			var sh = pixels.height;
-
-			var w = sw;
-			var h = sh;
-			var output = {
-				width: w, 
-				height: h, 
-				data: new Float32Array(w*h*4)
-			};
-			var dst = output.data;
-
-			var alphaFac = opaque ? 1 : 0;
-
-			for (var y=0; y<h; y++) {
-				for (var x=0; x<w; x++) {
-					var sy = y;
-					var sx = x;
-					var dstOff = (y*w+x)*4;
-					var r=0, g=0, b=0, a=0;
-					for (var cy=0; cy<side; cy++) {
-						for (var cx=0; cx<side; cx++) {
-							var scy = Math.min(sh-1, Math.max(0, sy + cy - halfSide));
-							var scx = Math.min(sw-1, Math.max(0, sx + cx - halfSide));
-							var srcOff = (scy*sw+scx)*4;
-							var wt = weights[cy*side+cx];
-							r += src[srcOff] * wt;
-							g += src[srcOff+1] * wt;
-							b += src[srcOff+2] * wt;
-							a += src[srcOff+3] * wt;
-						}
-					}
-					dst[dstOff] = (r > 255 ? 255 : r);
-					dst[dstOff+1] = (g > 255 ? 255 : g);
-					dst[dstOff+2] = (b > 255 ? 255 : b);
-					dst[dstOff+3] = (a + alphaFac*(255-a) > 255 ? 255 : a + alphaFac*(255-a));
-				}
-			}
-			return output;
-		}
-
 		this._original = function(context, img){
 			context.drawImage(img, 0, 0, _opt.width, _opt.height);
 		}
@@ -235,7 +189,7 @@
 			var height = el.height();
 			var parent = el.parent();
 
-			//el.remove();
+			el.remove();
 
 			// copy to global variable
 			_opt.height = height;
